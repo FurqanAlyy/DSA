8fdbd3ea92b0d7566bea70aa823498ac1db9da64
Furqan Ali <furqan24ali@gmail.com>
2026-09-16 20:07:26 +0500

    Time: 4 ms (93.82%), Space: 11.4 MB (46.15%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
+    vector<int> manacher(string& s) {
+        string t = "#" + string(1, s[0]);
+        for (int i = 1; i < s.size(); ++i)
+            t += "#" + string(1, s[i]);
+        t += "#";
+        int n = t.size();
+        vector<int> p(n, 0);
+        int l = 0, r = 0;
+        for (int i = 0; i < n; i++) {
+            p[i] = (i < r) ? min(r - i, p[l + (r - i)]) : 0;
+            while (i + p[i] + 1 < n && i - p[i] - 1 >= 0 &&
+                   t[i + p[i] + 1] == t[i - p[i] - 1])
+                p[i]++;
+            if (i + p[i] > r)
+                l = i - p[i], r = i + p[i];
+        }
+        return p;
+    }
+
     string longestPalindrome(string s) {
-        int resIdx = 0, resLen = 0;
-        int n = s.size();
-
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = i; j < n; j++) {
-                if (s[i] == s[j] &&
-                    (j - i <= 2 || dp[i + 1][j - 1])) {
-
-                    dp[i][j] = true;
-                    if (resLen < (j - i + 1)) {
-                        resIdx = i;
-                        resLen = j - i + 1;
-                    }
-                }
+        vector<int> p = manacher(s);
+        int resLen = 0, center_idx = 0;
+        for (int i = 0; i < p.size(); i++) {
+            if (p[i] > resLen) {
+                resLen = p[i];
+                center_idx = i;
             }
         }
-
+        int resIdx = (center_idx - resLen) / 2;
         return s.substr(resIdx, resLen);
     }
 };
